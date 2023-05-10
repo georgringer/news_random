cff3e86b2719adbd51b90c5833795cbe121de907
Daniel Schoeneck <whiteroom@users.noreply.github.com>
2023-05-10 15:24:16 +0200

    comp. tx_news Version 11 updated

--- Classes/Hooks/FlexFormHook.php
+++ Classes/Hooks/FlexFormHook.php
@@ -1,23 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace GeorgRinger\NewsRandom\Hooks;
 
+/**
+ * This file is part of the "news_random" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class FlexFormHook
 {
-    /**
-     * @param array $dataStructure
-     * @param array $identifier
-     * @return array
-     */
-    public function parseDataStructureByIdentifierPostProcess(array $dataStructure, array $identifier)
+    public function parseDataStructureByIdentifierPostProcess(array $dataStructure, array $identifier): array
     {
-        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === 'news_pi1,list') {
-            $file = PATH_site . 'typo3conf/ext/news_random/Configuration/Flexforms/flexform_news_random.xml';
+        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $identifier['dataStructureKey'] === '*,news_pi1') {
+            $file = ExtensionManagementUtility::extPath('news_random') . 'Configuration/Flexforms/flexform_news_random.xml';
             $content = file_get_contents($file);
             if ($content) {
-                $dataStructure['sheets']['extraEntryNewsRandom'] = GeneralUtility::xml2array($content);
+                $dataStructure['sheets']['extraEntryEventNews'] = GeneralUtility::xml2array($content);
             }
         }
         return $dataStructure;
